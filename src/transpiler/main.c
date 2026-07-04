d6125a03b2777eda3dcab88073802f60ae2575da
singlerr <singlerr@naver.com>
2026-07-04 21:18:10 +0900

    add compiler options

--- src/transpiler/main.c
+++ src/transpiler/main.c
@@ -1,12 +1,17 @@
+#include <getopt.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 #include "ast.h"
 #include "builtin_func.h"
-#include "type/tytab.h"
 #include "type/check.h"
+#include "type/tytab.h"
 
-/* forward declarations to avoid ast_sem.h/type/base.h struct conflict */
+#define HTAB "\t"
+#define NEWLINE "\n"
+#define FLAG_COMPILE (1)
+#define FLAG_OUTPUT (1 << 1)
+
 struct program;
 extern void convert_program (ast_t *root, struct program **out);
 extern void set_codegen_output (FILE *);
@@ -15,26 +20,78 @@ extern void gen_program (struct program *);
 extern ast_t *compile (FILE *);
 
 char *yyfilename;
+int flags;
+
+static struct option cmd_opts[] = { { "help", no_argument, 0, 'h' },
+                                    { "compile", required_argument, 0, 'c' },
+                                    { "output", required_argument, 0, 'o' } };
+
+static void
+print_usage (const char *file_name)
+{
+  printf ("Usage: %s [OPTION]..." NEWLINE, file_name);
+  printf (HTAB "--help : Specify the source file to compile [-h]" NEWLINE);
+  printf (HTAB "--compile : Specify the source file to compile [-c]" NEWLINE);
+}
 
 int
 main (int argc, char *argv[])
 {
   ast_t *root;
   struct program *sem_analysis;
-  FILE *f;
+  FILE *f, *o;
+  int opt;
+  optind = 1;
+  flags = 0;
 
-  if (argc > 1)
+  while ((opt = getopt_long (argc, argv, "hc:")) != -1)
     {
-      if ((f = fopen (argv[1], "r")) == NULL)
+      switch (opt)
         {
-          fprintf (stderr, "file open error for %s\n", argv[1]);
-          exit (1);
+        case 'h':
+          print_usage (argv[0]);
+          return 0;
+        case 'c':
+
+          if (optind >= argc)
+            {
+              fprintf (stderr, "Compile option(-c) requires an argument.\n");
+              return -1;
+            }
+
+          if ((f = fopen (argv[optind], "r")) == NULL)
+            {
+              fprintf (stderr, "%s does not exist.\n", argv[optind]);
+              return -1;
+            }
+
+          flags |= FLAG_COMPILE;
+          break;
+        case 'o':
+
+          if (optind >= argc)
+            {
+              fprintf (stderr, "Output option(-o) requires an argument.\n");
+              return -1;
+            }
+
+          if ((o = fopen (argv[optind], "w")) == NULL)
+            {
+              fprintf (stderr, "%s does not exist.\n", argv[optind]);
+              return -1;
+            }
+
+          flags |= FLAG_OUTPUT;
+          break;
+        default:
+          break;
         }
-      yyfilename = argv[1];
     }
-  else
+
+  if (!(flags & FLAG_COMPILE))
     {
-      f = stdin;
+      fprintf (stderr, "Specify a compile target");
+      return -1;
     }
 
   init_type_table ();
@@ -44,7 +101,6 @@ main (int argc, char *argv[])
   if (!root)
     exit (0);
 
-  /* Layer 3: annotate AST with types before lowering */
   {
     check_ctx_t ctx;
     ctx.fn_ret = NULL;
@@ -53,8 +109,17 @@ main (int argc, char *argv[])
   }
 
   convert_program (root, &sem_analysis);
-  set_codegen_output (stdout);
+
+  if (flags & FLAG_OUTPUT)
+    {
+      set_codegen_output (o);
+    }
+  else
+    {
+      set_codegen_output (stdout);
+    }
+
   gen_program (sem_analysis);
 
-  exit (0);
+  return 0;
 }
